24d6457d5f014e80b0218199a7bf25d69c307abd
“Prakrati” <PrakratiSharma@adt.com>
2025-02-17 13:23:25 -0500

    fixed: compile issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.maahiway.maahiwaynews"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.maahiway.maahiwaynews"
